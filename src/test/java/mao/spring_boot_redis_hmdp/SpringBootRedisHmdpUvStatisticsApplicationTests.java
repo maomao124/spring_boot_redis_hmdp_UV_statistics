88946a58cfbcd4bc599dfc860aa96709d517e73a
mao <1296193245@qq.com>
2022-05-20 22:06:49 +0800

    spring_boot_redis_hmdp_UV_statistics

--- src/test/java/mao/spring_boot_redis_hmdp/SpringBootRedisHmdpUvStatisticsApplicationTests.java
+++ src/test/java/mao/spring_boot_redis_hmdp/SpringBootRedisHmdpUvStatisticsApplicationTests.java
@@ -1,5 +1,6 @@
 package mao.spring_boot_redis_hmdp;
 
+import lombok.extern.slf4j.Slf4j;
 import mao.spring_boot_redis_hmdp.entity.Shop;
 import mao.spring_boot_redis_hmdp.service.IShopService;
 import mao.spring_boot_redis_hmdp.utils.RedisConstants;
@@ -16,6 +17,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 @SpringBootTest
+@Slf4j
 class SpringBootRedisHmdpUvStatisticsApplicationTests
 {
 
@@ -30,6 +32,9 @@ class SpringBootRedisHmdpUvStatisticsApplicationTests
     {
     }
 
+    /**
+     * 加载店铺信息到redis，按类型加载
+     */
     @Test
     void load()
     {
@@ -53,4 +58,31 @@ class SpringBootRedisHmdpUvStatisticsApplicationTests
         }
     }
 
+    /**
+     * 测试redis的uv统计功能
+     */
+    @Test
+    void UV_statistics()
+    {
+        //发送单位,当前为1000条发一次，如果没次都发送会大大增加网络io
+        int length = 1000;
+        //发送的总数，当前为一百万条数据
+        int total = 1000000;
+        int j = 0;
+        String[] values = new String[length];
+        for (int i = 0; i < total; i++)
+        {
+            j = i % length;
+            //赋值
+            values[j] = "user_" + i;
+            if (i == length - 1)
+            {
+                //发送到redis
+                stringRedisTemplate.opsForHyperLogLog().add("UV", values);
+            }
+        }
+        //发送完成，获得数据
+        Long size = stringRedisTemplate.opsForHyperLogLog().size("UV");
+        log.info("统计结果：" + size);
+    }
 }
